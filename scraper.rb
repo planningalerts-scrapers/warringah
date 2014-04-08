8302177e0901ddba83799b9c9b5de5401979d300
Matthew Landauer <matthew@openaustralia.org>
2014-04-08 16:36:15 +1000

    Should work now

--- scraper.rb
+++ scraper.rb
@@ -3,22 +3,20 @@ require 'scraperwiki'
 
 # XML feed of the applications submitted in the last 14 days
 base_url = "http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track"
-url = base_url + "/SearchApplication.aspx?o=xml&d=last14days"
+url = base_url + "/SearchApplication.aspx?o=xml&d=last14days&t=DevApp"
 
 agent = Mechanize.new
 page = Nokogiri::XML(agent.get(url).body)
-
 page.search("Application").each do |app|
   record = {
     "council_reference" => app.at('ReferenceNumber').inner_text,
     "date_received" => Date.parse(app.at('LodgementDate').inner_text).to_s,
+    "date_scraped" => Date.today.to_s
   }
   record["info_url"] =  base_url + "/SearchApplication.aspx?id=" + record["council_reference"]
   record["comment_url"] = base_url + "/Submission.aspx?id=" + record['council_reference']
   # Only use the first address
-  if app.at('Address')
-    record["address"] = app.at('Address Line1').inner_text + ", " + app.at('Address Line2').inner_text
-  end
+  record["address"] = app.at('Address Line1').inner_text + ", " + app.at('Address Line2').inner_text
   # Some DAs have good descriptions whilst others just have
   # "<insert here>" so we search for "<insert" and if it's there we
   # use another more basic description
