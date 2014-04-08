cb8b170f477f793c0e827ef378fcd5d49ea1a053
Matthew Landauer <matthew@openaustralia.org>
2014-04-08 16:23:54 +1000

    Convert to form for morph

--- scraper.rb
+++ scraper.rb
@@ -1,29 +1,36 @@
-require 'scraper'
+require 'mechanize'
+require 'scraperwiki'
 
-class WarringahScraper < Scraper
-  def applications(date)
-    # XML feed of the applications submitted in the last 14 days
-    url = "http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track/SearchApplication.aspx?o=xml&d=last14days&t=DevApp"
-    page = Nokogiri::XML(agent.get(url).body)
-    page.search('Application').map do |app|
-      data = {
-        :info_url => "http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track/SearchApplication.aspx?id=" + app.at('ApplicationId').inner_text,
-        :comment_url => "http://eservices2.warringah.nsw.gov.au/ePlanning/Pages/XC.Track/Submission.aspx?id=#{app.at('ApplicationId').inner_text}",
-        :application_id => app.at('ReferenceNumber').inner_text,
-        :date_received => Date.parse(app.at('LodgementDate').inner_text)
-      }
-      # Some DAs have good descriptions whilst others just have
-      # "<insert here>" so we search for "<insert" and if it's there we
-      # use another more basic description
-      if app.at('ApplicationDetails').nil? || app.at('ApplicationDetails').inner_text.downcase.index(/(<|\()insert/)
-        data[:description] = app.at('NatureOfApplication').inner_text
-      else
-        data[:description] = app.at('ApplicationDetails').inner_text
-      end
-      data[:addresses] = app.xpath('Address').map do |address|
-        address.at('Line1').inner_text + ", " + address.at('Line2').inner_text
-      end
-      DevelopmentApplication.new(data)
-    end
+# XML feed of the applications submitted in the last 14 days
+base_url = "http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track"
+url = base_url + "/SearchApplication.aspx?o=xml&d=last14days"
+
+agent = Mechanize.new
+page = Nokogiri::XML(agent.get(url).body)
+
+page.search("Application").each do |app|
+  record = {
+    "council_reference" => app.at('ReferenceNumber').inner_text,
+    "date_received" => Date.parse(app.at('LodgementDate').inner_text).to_s,
+  }
+  record["info_url"] =  base_url + "/SearchApplication.aspx?id=" + record["council_reference"]
+  record["comment_url"] = base_url + "/Submission.aspx?id=" + record['council_reference']
+  # Only use the first address
+  if app.at('Address')
+    record["address"] = app.at('Address Line1').inner_text + ", " + app.at('Address Line2').inner_text
+  end
+  # Some DAs have good descriptions whilst others just have
+  # "<insert here>" so we search for "<insert" and if it's there we
+  # use another more basic description
+  if app.at('ApplicationDetails').nil? || app.at('ApplicationDetails').inner_text.downcase.index(/(<|\()insert/)
+    record["description"] = app.at('NatureOfApplication').inner_text
+  else
+    record["description"] = app.at('ApplicationDetails').inner_text
+  end
+  #p record
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+  ScraperWiki.save_sqlite(['council_reference'], record)
+  else
+  puts "Skipping already saved record " + record['council_reference']
   end
 end
