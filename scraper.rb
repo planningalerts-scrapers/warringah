9df2d2f082638913df3e54c6ff4b58e33cc2882b
Luke Bacon <luke@equivalentideas.com>
2015-06-16 17:42:07 +1000

    Update url

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,4 @@ require 'scraperwiki'
 require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
 # XML feed of the applications submitted in the last 14 days
-scrape_icon_rest_xml("http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track/SearchApplication.aspx", "o=xml&d=last14days&t=DevApp")
-
+scrape_icon_rest_xml("https://eservices1.warringah.nsw.gov.au/ePlanning/live/Public/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=161&o=xml")
