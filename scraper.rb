5da2fb1cfa48c429dca36dadb78923fd3b7faf79
Luke Bacon <luke@equivalentideas.com>
2015-06-16 17:41:14 +1000

    Use the xml reading library rather than custom code

--- scraper.rb
+++ scraper.rb
@@ -1,34 +1,6 @@
-require 'mechanize'
 require 'scraperwiki'
+require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
 # XML feed of the applications submitted in the last 14 days
-base_url = "http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track"
-url = base_url + "/SearchApplication.aspx?o=xml&d=last14days&t=DevApp"
+scrape_icon_rest_xml("http://eservices2.warringah.nsw.gov.au/ePlanning/Public/XC.Track/SearchApplication.aspx", "o=xml&d=last14days&t=DevApp")
 
-agent = Mechanize.new
-page = Nokogiri::XML(agent.get(url).body)
-page.search("Application").each do |app|
-  record = {
-    "council_reference" => app.at('ReferenceNumber').inner_text,
-    "date_received" => Date.parse(app.at('LodgementDate').inner_text).to_s,
-    "date_scraped" => Date.today.to_s
-  }
-  record["info_url"] =  base_url + "/SearchApplication.aspx?id=" + record["council_reference"]
-  record["comment_url"] = base_url + "/Submission.aspx?id=" + record['council_reference']
-  # Only use the first address
-  record["address"] = app.at('Address Line1').inner_text + ", " + app.at('Address Line2').inner_text
-  # Some DAs have good descriptions whilst others just have
-  # "<insert here>" so we search for "<insert" and if it's there we
-  # use another more basic description
-  if app.at('ApplicationDetails').nil? || app.at('ApplicationDetails').inner_text.downcase.index(/(<|\()insert/)
-    record["description"] = app.at('NatureOfApplication').inner_text
-  else
-    record["description"] = app.at('ApplicationDetails').inner_text
-  end
-  #p record
-  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-  ScraperWiki.save_sqlite(['council_reference'], record)
-  else
-  puts "Skipping already saved record " + record['council_reference']
-  end
-end
